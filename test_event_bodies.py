aa7f990ef0a42a24eef30c15a727d0054043b150
Chelsea Dole <chelseadole@ip-10-165-71-98.ec2.internal>
2019-10-17 09:37:36 -0700

    adding a large_event_body

--- test_event_bodies.py
+++ test_event_bodies.py
@@ -12,5 +12,212 @@ event_bodies = {
         "longitude": 2.0,
         "approved": True
     },
-    "large_event": {}
+    "large_event": {
+        "1": "The blacking-warehouse was the last house on the left-hand side of the way, at old Hungerford Stairs. It"
+             "was a crazy, tumble-down old house, abutting of course on the river, and literally overrun with rats."
+             "Its wainscoted rooms, and its rotten floors and staircase, and the old grey rats swarming down in the"
+             "cellars, and the sound of their squeaking and scuffling coming up the stairs at all times, and the dirt"
+             "and decay of the place, rise up visibly before me, as if I were there again. The counting-house was on"
+             "the first floor, looking over the coal-barges and the river. There was a recess in it, in which I was to"
+             "sit and work. My work was to cover the pots of paste-blacking; first with a piece of oil-paper, and then"
+             "with a piece of blue paper; to tie them round with a string; and then to clip the paper close and neat,"
+             "all round, until it looked as smart as a pot of ointment from an apothecary's shop. When a certain number"
+             "of grosses of pots had attained this pitch of perfection, I was to paste on each a printed label, and"
+             "then go on again with more pots. Two or three other boys were kept at similar duty down-stairs on similar"
+             "wages. One of them came up, in a ragged apron and a paper cap, on the first Monday morning, to show me"
+             "the trick of using the string and tying the knot. His name was Bob Fagin; and I took the liberty of using"
+             "his name, long afterwards, in Oliver Twist.",
+        "2": 2000,
+        "3": "m",
+        "4": 4000,
+        "5": "test_0 test_1 test_2 test_3 test_4 test_5 test_6 test_7 test_8 test_9 test_10 test_11 test_12 test_13"
+             "test_14 test_15 test_16 test_17 test_18 test_19",
+        "6": 6000,
+        "7": 7000,
+        "8": "test",
+        "9": 9.0,
+        "10": 10.0,
+        "11": True,
+        "12": "test test test",
+        "13": 13000,
+        "14": "f",
+        "15": 15000,
+        "16": "woohoooo",
+        "17": 17000,
+        "18": 18000,
+        "19": "test_0 test_1 test_2 test_3 test_4 test_5 test_6 test_7 test_8 test_9 test_10 test_11 test_12 test_13"
+             "test_14 test_15 test_16 test_17 test_18 test_19",
+        "20": 20.5,
+        "21": 21.0,
+        "22": False,
+        "23": "The blacking-warehouse was the last house on the left-hand side of the way, at old Hungerford Stairs. It"
+             "was a crazy, tumble-down old house, abutting of course on the river, and literally overrun with rats."
+             "Its wainscoted rooms, and its rotten floors and staircase, and the old grey rats swarming down in the"
+             "cellars, and the sound of their squeaking and scuffling coming up the stairs at all times, and the dirt"
+             "and decay of the place, rise up visibly before me, as if I were there again. The counting-house was on"
+             "the first floor, looking over the coal-barges and the river. There was a recess in it, in which I was to"
+             "sit and work. My work was to cover the pots of paste-blacking; first with a piece of oil-paper, and then"
+             "with a piece of blue paper; to tie them round with a string; and then to clip the paper close and neat,"
+             "all round, until it looked as smart as a pot of ointment from an apothecary's shop. When a certain number"
+             "of grosses of pots had attained this pitch of perfection, I was to paste on each a printed label, and"
+             "then go on again with more pots. Two or three other boys were kept at similar duty down-stairs on similar"
+             "wages. One of them came up, in a ragged apron and a paper cap, on the first Monday morning, to show me"
+             "the trick of using the string and tying the knot. His name was Bob Fagin; and I took the liberty of using"
+             "his name, long afterwards, in Oliver Twist.",
+        "24": 2400000,
+        "25": "f",
+        "26": 2600,
+        "27": "The blacking-warehouse was the last house on the left-hand side of the way, at old Hungerford Stairs. It"
+             "was a crazy, tumble-down old house, abutting of course on the river, and literally overrun with rats."
+             "Its wainscoted rooms, and its rotten floors and staircase, and the old grey rats swarming down in the"
+             "cellars, and the sound of their squeaking and scuffling coming up the stairs at all times, and the dirt"
+             "and decay of the place, rise up visibly before me, as if I were there again. The counting-house was on"
+             "the first floor, looking over the coal-barges and the river. There was a recess in it, in which I was to"
+             "sit and work. My work was to cover the pots of paste-blacking; first with a piece of oil-paper, and then"
+             "with a piece of blue paper; to tie them round with a string; and then to clip the paper close and neat,"
+             "all round, until it looked as smart as a pot of ointment from an apothecary's shop. When a certain number"
+             "of grosses of pots had attained this pitch of perfection, I was to paste on each a printed label, and"
+             "then go on again with more pots. Two or three other boys were kept at similar duty down-stairs on similar"
+             "wages. One of them came up, in a ragged apron and a paper cap, on the first Monday morning, to show me"
+             "the trick of using the string and tying the knot. His name was Bob Fagin; and I took the liberty of using"
+             "his name, long afterwards, in Oliver Twist.",
+        "28": 28000,
+        "29": 29000,
+        "30": "thirty",
+        "31": 31.05,
+        "32": 32.00,
+        "33": False,
+        "34": "test_0 test_1 test_2 test_3 test_4 test_5 test_6 test_7 test_8 test_9 test_10 test_11 test_12 test_13"
+             "test_14 test_15 test_16 test_17 test_18 test_19",
+        "35": 35900,
+        "36": "m",
+        "37": 37500,
+        "38": "a test",
+        "39": 39000,
+        "40": 40000,
+        "41": "a number",
+        "42": 421.0,
+        "43": 322.2,
+        "44": True,
+        "45": "test_0 test_1 test_2 test_3 test_4 test_5 test_6 test_7 test_8 test_9 test_10 test_11 test_12 test_13"
+             "test_14 test_15 test_16 test_17 test_18 test_19",
+        "46": 46000,
+        "47": "f",
+        "48": 48000,
+        "49": "strrrrrr",
+        "50": 500,
+        "51": 51111,
+        "52": "The blacking-warehouse was the last house on the left-hand side of the way, at old Hungerford Stairs. It"
+             "was a crazy, tumble-down old house, abutting of course on the river, and literally overrun with rats."
+             "Its wainscoted rooms, and its rotten floors and staircase, and the old grey rats swarming down in the"
+             "cellars, and the sound of their squeaking and scuffling coming up the stairs at all times, and the dirt"
+             "and decay of the place, rise up visibly before me, as if I were there again. The counting-house was on"
+             "the first floor, looking over the coal-barges and the river. There was a recess in it, in which I was to"
+             "sit and work. My work was to cover the pots of paste-blacking; first with a piece of oil-paper, and then"
+             "with a piece of blue paper; to tie them round with a string; and then to clip the paper close and neat,"
+             "all round, until it looked as smart as a pot of ointment from an apothecary's shop. When a certain number"
+             "of grosses of pots had attained this pitch of perfection, I was to paste on each a printed label, and"
+             "then go on again with more pots. Two or three other boys were kept at similar duty down-stairs on similar"
+             "wages. One of them came up, in a ragged apron and a paper cap, on the first Monday morning, to show me"
+             "the trick of using the string and tying the knot. His name was Bob Fagin; and I took the liberty of using"
+             "his name, long afterwards, in Oliver Twist.",
+        "53": 53.0,
+        "54": 54.0,
+        "55": False,
+        "56": "strringz",
+        "57": 57,
+        "58": "f",
+        "59": 59,
+        "60": "test_0 test_1 test_2 test_3 test_4 test_5 test_6 test_7 test_8 test_9 test_10 test_11 test_12 test_13"
+             "test_14 test_15 test_16 test_17 test_18 test_19",
+        "61": 61,
+        "62": 62,
+        "63": "string",
+        "64": 64.0,
+        "65": 65.0,
+        "66": True,
+        "67": "f",
+        "68": 68,
+        "69": "test_0 test_1 test_2 test_3 test_4 test_5 test_6 test_7 test_8 test_9 test_10 test_11 test_12 test_13"
+             "test_14 test_15 test_16 test_17 test_18 test_19",
+        "70": 70,
+        "71": 71,
+        "72": "The blacking-warehouse was the last house on the left-hand side of the way, at old Hungerford Stairs. It"
+             "was a crazy, tumble-down old house, abutting of course on the river, and literally overrun with rats."
+             "Its wainscoted rooms, and its rotten floors and staircase, and the old grey rats swarming down in the"
+             "cellars, and the sound of their squeaking and scuffling coming up the stairs at all times, and the dirt"
+             "and decay of the place, rise up visibly before me, as if I were there again. The counting-house was on"
+             "the first floor, looking over the coal-barges and the river. There was a recess in it, in which I was to"
+             "sit and work. My work was to cover the pots of paste-blacking; first with a piece of oil-paper, and then"
+             "with a piece of blue paper; to tie them round with a string; and then to clip the paper close and neat,"
+             "all round, until it looked as smart as a pot of ointment from an apothecary's shop. When a certain number"
+             "of grosses of pots had attained this pitch of perfection, I was to paste on each a printed label, and"
+             "then go on again with more pots. Two or three other boys were kept at similar duty down-stairs on similar"
+             "wages. One of them came up, in a ragged apron and a paper cap, on the first Monday morning, to show me"
+             "the trick of using the string and tying the knot. His name was Bob Fagin; and I took the liberty of using"
+             "his name, long afterwards, in Oliver Twist.",
+        "73": 73.0,
+        "74": 74.0,
+        "75": False,
+        "76": "string",
+        "77": 77,
+        "78": "m",
+        "79": 79,
+        "80": "string",
+        "81": 81,
+        "82": 82,
+        "83": "test_0 test_1 test_2 test_3 test_4 test_5 test_6 test_7 test_8 test_9 test_10 test_11 test_12 test_13"
+             "test_14 test_15 test_16 test_17 test_18 test_19",
+        "84": 84.0,
+        "85": 85.0,
+        "86": True,
+        "87": "string",
+        "88": 88,
+        "89": "f",
+        "90": 90,
+        "91": "string",
+        "92": 92,
+        "93": 93,
+        "94": "test_0 test_1 test_2 test_3 test_4 test_5 test_6 test_7 test_8 test_9 test_10 test_11 test_12 test_13"
+             "test_14 test_15 test_16 test_17 test_18 test_19",
+        "95": 95.0,
+        "96": 96.0,
+        "97": True,
+        "98": "string",
+        "99": 99,
+        "100": "test",
+        "101": 101,
+        "102": "The blacking-warehouse was the last house on the left-hand side of the way, at old Hungerford Stairs.It"
+             "was a crazy, tumble-down old house, abutting of course on the river, and literally overrun with rats."
+             "Its wainscoted rooms, and its rotten floors and staircase, and the old grey rats swarming down in the"
+             "cellars, and the sound of their squeaking and scuffling coming up the stairs at all times, and the dirt"
+             "and decay of the place, rise up visibly before me, as if I were there again. The counting-house was on"
+             "the first floor, looking over the coal-barges and the river. There was a recess in it, in which I was to"
+             "sit and work. My work was to cover the pots of paste-blacking; first with a piece of oil-paper, and then"
+             "with a piece of blue paper; to tie them round with a string; and then to clip the paper close and neat,"
+             "all round, until it looked as smart as a pot of ointment from an apothecary's shop. When a certain number"
+             "of grosses of pots had attained this pitch of perfection, I was to paste on each a printed label, and"
+             "then go on again with more pots. Two or three other boys were kept at similar duty down-stairs on similar"
+             "wages. One of them came up, in a ragged apron and a paper cap, on the first Monday morning, to show me"
+             "the trick of using the string and tying the knot. His name was Bob Fagin; and I took the liberty of using"
+             "his name, long afterwards, in Oliver Twist.",
+        "103": 103,
+        "104": 104,
+        "105": "string",
+        "106": 106.0,
+        "107": 107.0,
+        "108": True,
+        "109": "string",
+        "110": 110,
+        "111": "m",
+        "112": 112,
+        "113": "string",
+        "114": 114,
+        "115": 115,
+        "116": "string",
+        "117": 117.0,
+        "118": 118.0,
+        "119": "string",
+        "120": True
+    }
 }
